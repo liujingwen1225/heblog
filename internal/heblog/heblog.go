7d2b885d5b0b039baae13c786b44e8cd928c3004
liujingwen <286618724@qq.com>
2023-02-12 23:02:08 +0800

    feat: 添加版本信息

--- internal/heblog/heblog.go
+++ internal/heblog/heblog.go
@@ -3,9 +3,11 @@ package heblog
 import (
 	"encoding/json"
 	"fmt"
+	"heblog/pkg/log"
+	"heblog/pkg/version/verflag"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"heblog/internal/pkg/log"
 )
 
 var cfgFile string
@@ -14,21 +16,26 @@ var cfgFile string
 func NewMiniBlogCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		// 指定命令的名字，该名字会出现在帮助信息中
-		Use: "heblog",
+		Use: "miniblog",
 		// 命令的简短描述
 		Short: "A good Go practical project",
 		// 命令的详细描述
 		Long: `A good Go practical project, used to create user with basic information.
-Find more heblog information at:
-https://github.com/marmotedu/heblog#readme`,
+
+Find more miniblog information at:
+	https://github.com/marmotedu/miniblog#readme`,
 
 		// 命令出错时，不打印帮助信息。不需要打印帮助信息，设置为 true 可以保持命令出错时一眼就能看到错误信息
 		SilenceUsage: true,
 		// 指定调用 cmd.Execute() 时，执行的 Run 函数，函数执行失败会返回错误信息
 		RunE: func(cmd *cobra.Command, args []string) error {
+			// 如果 `--version=true`，则打印版本并退出
+			verflag.PrintAndExitIfRequested()
+
 			// 初始化日志
 			log.Init(logOptions())
 			defer log.Sync() // Sync 将缓存中的日志刷新到磁盘文件中
+
 			return run()
 		},
 		// 这里设置命令运行时，不需要指定命令行参数
@@ -42,17 +49,30 @@ https://github.com/marmotedu/heblog#readme`,
 			return nil
 		},
 	}
+
+	// 以下设置，使得 initConfig 函数在每个命令运行时都会被调用以读取配置
 	cobra.OnInitialize(initConfig)
-	cmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "The path to the heblog configuration file. Empty string for no configuration file.")
+
+	// 在这里您将定义标志和配置设置。
+
+	// Cobra 支持持久性标志(PersistentFlag)，该标志可用于它所分配的命令以及该命令下的每个子命令
+	cmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "The path to the miniblog configuration file. Empty string for no configuration file.")
+
+	// Cobra 也支持本地标志，本地标志只能在其所绑定的命令上使用
 	cmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	// 添加 --version 标志
+	verflag.AddFlags(cmd.PersistentFlags())
+
 	return cmd
 }
 
 // run 函数是实际的业务代码入口函数.
 func run() error {
-	log.Infow("Hello MiniBlog!")
+	// 打印所有的配置项及其值
 	settings, _ := json.Marshal(viper.AllSettings())
 	log.Infow(string(settings))
+	// 打印 db -> username 配置项的值
 	log.Infow(viper.GetString("db.username"))
 	return nil
 }
