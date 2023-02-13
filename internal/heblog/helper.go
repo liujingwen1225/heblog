fc5029f9725f8a2e65647e2f883cf5f75bca1ae7
liujingwen <286618724@qq.com>
2023-02-13 20:36:02 +0800

    中间件添加

--- internal/heblog/helper.go
+++ internal/heblog/helper.go
@@ -3,7 +3,7 @@ package heblog
 import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	log2 "heblog/pkg/log"
+	"heblog/internal/pkg/log"
 	"os"
 	"path/filepath"
 	"strings"
@@ -42,17 +42,17 @@ func initConfig() {
 
 	// 读取配置文件。如果指定了配置文件名，则使用指定的配置文件，否则在注册的搜索路径中搜索
 	if err := viper.ReadInConfig(); err != nil {
-		log2.Errorw("Failed to read viper configuration file", "err", err)
+		log.Errorw("Failed to read viper configuration file", "err", err)
 	}
 
 	// 打印 viper 当前使用的配置文件，方便 Debug.
-	log2.Infow("Using config file", "file", viper.ConfigFileUsed())
+	log.Infow("Using config file", "file", viper.ConfigFileUsed())
 }
 
 // logOptions 从 viper 中读取日志配置，构建 `*log.Options` 并返回.
 // 注意：`viper.Get<Type>()` 中 key 的名字需要使用 `.` 分割，以跟 YAML 中保持相同的缩进.
-func logOptions() *log2.Options {
-	return &log2.Options{
+func logOptions() *log.Options {
+	return &log.Options{
 		DisableCaller:     viper.GetBool("log.disable-caller"),
 		DisableStacktrace: viper.GetBool("log.disable-stacktrace"),
 		Level:             viper.GetString("log.level"),
